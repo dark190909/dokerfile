64e2469bab27007ecd6b409e6e99ad9005c1d836
Emil Mamedov <dark85792@gmail.com>
2025-04-04 00:44:21 +0600

    Initial commit

---
+++ src/com/example/java/App.java
@@ -0,0 +1,17 @@
+package com.example.java;
+
+/**
+ * Класс-пример для генерации Javadoc.
+ *
+ * @author John Doe
+ * @version 1.0
+ */
+public class App {
+    /**
+     * Главный метод программы.
+     * @param args Аргументы командной строки
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Javadoc!");
+    }
+}
